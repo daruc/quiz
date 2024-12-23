29f01d8d7fafe3f31ef27afca4dac1cb20c7ae0d
Dariusz Ruchała <darek40bc@gmail.com>
2024-12-23 14:27:28 +0100

    Added empty back end project

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
-
-export const routes: Routes = [
-    { path: '', component: LayoutComponent },
-    { path: 'quiz/:id/:page', component: LayoutComponent },
-    { path: 'create', component: LayoutComponent },
-    { path: 'edit/:id', component: LayoutComponent }
-];
